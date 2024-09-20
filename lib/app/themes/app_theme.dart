1b60f0b1fbb96bf6593fa62b1f83b3ac3d7f8f03
Priyanka <pr55@witty.tech>
2024-09-20 16:37:07 +0530

    Deploy web app to GitHub Pages

--- lib/app/themes/app_theme.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-
-class AppTheme {
-  static final ThemeData light = ThemeData(
-    primarySwatch: Colors.blue,
-    visualDensity: VisualDensity.adaptivePlatformDensity,
-  );
-}
--- lib/utils/constants.dart
+++
@@ -1,4 +0,0 @@
-class Constants {
-  static const String pixabayBaseUrl = 'https://pixabay.com/api/';
-  static const String pixabayApiKey = '46086839-f75937fdf2e35e7f4c29cde08';
-}
